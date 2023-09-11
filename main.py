686e9c84b44d750fc8e6245f928e9d8edf79b93d
Christopher Partin <christopherpartin11@gmail.com>
2023-09-11 15:53:39 -0700

    Just added a brief main function on main.py with a bit of dialogue from the game, as well as an input for the player to insert their name.

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+def main():
+    player_name = input("Hello. My name is Chester, the Hangman Guru. Isn't Hangman the best? So much strategy, "
+                        "so many delightful choices. Oh, I'm sorry. Where are my manners. What is your name? \n \n")
+
+    print("Nice to meet you, " + player_name + ". I would love to a play a game of Hangman against you.")
